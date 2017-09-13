de1aba162367fd50f4d96bd570245acc7f9f742e
Eogan Sullivan <eogan.sullivan@outlook.com>
2017-09-13 18:58:47 +0100

    Practical1
    -Added Ball and Plane to Scene
    -Added mesh/materials and colliders to above
    -Added second ball
    - Added basic physics to second ball

---
+++ Assets/SpherePhysicsCode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpherePhysicsCode : MonoBehaviour {
+
+    Vector3 acceleration,velocity;
+    float cor = 0.5f; //Co- Eficent of Restitioun
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        acceleration = /*Physics.gravity;*/ 9.8f * Vector3.down; 
+        velocity += acceleration * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+
+        if (transform.position.y < 0.5f)
+        {
+            transform.position -= velocity * Time.deltaTime;
+            velocity = - cor * velocity;
+        }
+		
+	}
+}
